8a319ab1a8d1cde6ee85bf2c5b7e3cd75baf2105
Nishan Singh <nishankhiva@gmail.com>
2025-06-25 12:30:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const cusAccountApi = require('./router/CusAccountRouter');
 const app = express();
 app.use(cors());
 app.use(express.json()); 
-const PORT = process.env.PORT || 4000; 
+const PORT = process.env.PORT; 
 
 // app.use('/api', registerRouter);
 app.use('/api/emply', employeeApi)
